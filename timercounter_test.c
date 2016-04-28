07adb6481aa3dfb59427a69694ba536e0078c8c0
Colin Comard <colin.comard@gmail.com>
2016-04-28 15:38:42 -0700

    Transmission working?

--- timercounter_test.c
+++ timercounter_test.c
@@ -1,5 +1,6 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <util/delay_basic.h>
 
 #define CPU_PRESCALE(n) (CLKPR = 0x80, CLKPR = (n))
 #define CPU_16MHz       0x00
@@ -15,11 +16,9 @@
 
 #define true 1
 // initialize timer, interrupt and variable
-void timer0_init()
-{
-    // set up timer with prescaler = 64 and CTC mode
+void timer0_init(){
+    // set up timer with clock off and CTC mode
     TCCR0A |= (0 << COM0A1)|(1 << COM0A0)|(0 << COM0B1)|(1<<COM0B0)|(1<<WGM01)|(0<<WGM00);
-	TCCR0B |= (1 << CS01);
   
     // initialize counter
     TCNT0 = 0;
@@ -27,12 +26,31 @@ void timer0_init()
     // initialize compare value
     OCR0A = 39;
 }
-  
-int main(void)
-{
-	CPU_PRESCALE(CPU_16MHz);
 
-    DDRB = 0xFF;
+void timer1_init(){
+	// Set timer settings (Do Not Turn On)
+	TCCR1A = 0x00; // Only used for counting, so ensure eveything is default
+	TCNT1 = 0;
+}
+
+/*
+ *  Function for transmitting sonar pulses. Works as follows:
+ *  1) Turn on Timer/Counter 0
+ *  2) Run for ~8 wavelengths
+ *  3) Turn off Timer/Counter 0, set output to low
+ */
+void transmit(){
+	TCNT0 = 0;
+	TCCR0B |= (1 << CS01);
+	_delay_loop_2(1280);
+	TCCR0B &= ~(1 << CS01);
+	PORTB &= ~(1 << PB7);
+}
+  
+int main(void){
+	CPU_PRESCALE(CPU_16MHz);
+	
+    DDRB = 0x80;
   
     // initialize timer
     timer0_init();
@@ -41,8 +59,9 @@ int main(void)
     // loop forever
     while(1)
     {
-/*		if(TIFR0 & (1 << OCF0A))
-			PORTB ^= (1 << 0)|(1 << 1);
-		TIFR0 |= (1 << OCF1A);*/
+		if(PORTB & 1 << 0){
+			transmit();	
+			
+		}
     }
 }
